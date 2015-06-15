f27ee5e7bb2aa8d10781e420e0fc7f6010ea30e2
Will Stamper <epmatsw@gmail.com>
2015-06-15 13:49:33 -0700

    install script

---
+++ install-plugin.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/tnwinc/omz-git.git ${ZSH}/custom/plugins/omz-git.plugin.zsh
+
+echo "Open your .zshrc file and add omz-git to your plugins."
+echo "You should end up with something like plugins=(git omz-git)."
+echo "Then, either restart your terminal or run source ~/.zshrc"
